d33dcf31cf71df39ff488ea4864bfcb3df4a1838
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2015-08-18 01:20:38 +0900

    add choreonoid.launch and update run_choreonoid.sh for parsing rtm options

---
+++ hrpsys_choreonoid/launch/run_choreonoid.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cnoid_proj=""
+if [ "$(echo $1 | grep \.cnoid$ | wc -l)" == 1 ]; then
+    cnoid_proj=$1
+fi
+
+rtm_args=()
+latch=0
+
+for arg in $@; do
+## debug
+##  echo $arg 1>&2;
+  if [ $latch == 1 ]; then
+      rtm_args=("${rtm_args[@]}" $arg)
+      latch=0
+  fi
+  if [ $arg == "-o" ]; then
+      latch=1
+  fi
+done
+
+## debug
+##echo "${rtm_args[@]}" 1>&2
+
+rtc_conf="/tmp/rtc.conf.choreonoid"
+if [ -e $rtc_conf ]; then
+    rm -rf $rtc_conf
+fi
+
+for arg in "${rtm_args[@]}";
+  do echo $arg >> $rtc_conf;
+done
+
+echo "$ choreonoid $cnoid_proj" 1>&2
+echo "with rtc.conf file on $rtc_conf" 1>&2
+echo "<BEGIN: rtc.conf>"
+cat $rtc_conf 1>&2
+echo "<END: rtc.conf>"
+
+(cd /tmp; choreonoid $cnoid_proj)
