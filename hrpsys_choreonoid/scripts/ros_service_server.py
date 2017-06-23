cf61ebba2fbaf0482611628fce7b52cc0a1e37b9
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2017-06-23 12:33:48 +0900

    [hrpsys_choreonoid] update ros_service_server

--- hrpsys_choreonoid/scripts/ros_service_server.py
+++ hrpsys_choreonoid/scripts/ros_service_server.py
@@ -1,6 +1,8 @@
 import cnoid.Base
 import cnoid.BodyPlugin
 import cnoid.Util
+#from cnoid.OpenRTMPlugin import *
+#from cnoid.PythonSimScriptPlugin import *
 import numpy, time, math
 
 import sys
@@ -8,6 +10,8 @@ setattr(sys, 'argv', [''])
 
 import rospy
 from roseus.srv import *
+
+thisSimulatorItem = None
 
 def roscallback (req):
     ret = 'not called'
@@ -24,29 +28,36 @@ def ros_service_init ():
 
 def addExternalForce(robotname = "JAXON_RED", linkname = "WAIST", pos = [0,0,1.0], force = [100,0,0], tm = 0.2):
     #addExternalForce("JAXON_RED", "WAIST", [0, 0, 1.0], [100, 0, 0], 0.2)
+    global thisSimulatorItem
+
     robotItem = cnoid.Base.RootItem.instance().findItem(robotname)
     if robotItem == None:
         return 'invalid robotname %s'%(robotname)
 
-    simulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
-    if simulatorItem == None:
-        return 'invalid simulatorItem'
+    if thisSimulatorItem == None:
+        thisSimulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
+        if thisSimulatorItem == None:
+            return 'invalid simulatorItem'
 
     pushingLink = robotItem.body().link(linkname)
     if pushingLink:
-        simulatorItem.setExternalForce(robotItem, pushingLink, pos, force, tm)
+        thisSimulatorItem.setExternalForce(robotItem, pushingLink, pos, force, tm)
         return 'success'
+
     return 'invalid link: %s'%(linkname)
 
 def resetPosition(robotname = "JAXON_RED", pos = [0,0,1.0], rpy = [0,0,0], sleep = 0.2):
     #resetPosition("JAXON_RED", [0, 0, 1.0], [0, 0, 0])
+    global thisSimulatorItem
+
     robotItem = cnoid.Base.RootItem.instance().findItem(robotname)
     if robotItem == None:
         return 'invalid robotname %s'%(robotname)
 
-    simulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
-    if simulatorItem == None:
-        return 'invalid simulatorItem'
+    if thisSimulatorItem == None:
+        thisSimulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
+        if thisSimulatorItem == None:
+            return 'invalid simulatorItem'
 
     mat = cnoid.Util.rotFromRpy(rpy)
 
@@ -54,11 +65,60 @@ def resetPosition(robotname = "JAXON_RED", pos = [0,0,1.0], rpy = [0,0,0], sleep
                        [mat[1][0], mat[1][1], mat[1][2], pos[1]],
                        [mat[2][0], mat[2][1], mat[2][2], pos[2]]])
 
-    simulatorItem.setForcedPosition(robotItem, trs)
+    thisSimulatorItem.setForcedPosition(robotItem, trs)
     ##
     time.sleep(sleep) ##
-    simulatorItem.clearForcedPositions()
+    thisSimulatorItem.clearForcedPositions()
 
     return 'success'
+
+def addObject(objname, filename, translation = None, rotation = None):
+    global thisSimulatorItem
+
+    itemTreeView = cnoid.Base.ItemTreeView.instance()
+    rootItem = cnoid.Base.RootItem.instance()
+
+    world = rootItem.findItem("World")
+    if world == None:
+        return 'world not found'
+
+    robotItem = cnoid.BodyPlugin.BodyItem()
+    robotItem.load(filename)
+    robotItem.setName(objname)
+
+    robot = robotItem.body()
+    if robot == None:
+        return 'file not found'
+
+    if translation:
+        robot.rootLink().setTranslation(translation);
+    if rotation:
+        robot.rootLink().setRotation(rotation);
+
+    for i in range(robot.numJoints()):
+        robot.joint(i).q = 0
+
+    robot.calcForwardKinematics()
+    robotItem.storeInitialState()
+    world.insertChildItem(robotItem, world.childItem())
+    itemTreeView.checkItem(robotItem)
+
+    return 'success'
+
+def callSimulation(robotname = "JAXON_RED", call = 'pauseSimulation'):
+    global thisSimulatorItem
+
+    robotItem = cnoid.Base.RootItem.instance().findItem(robotname)
+    if robotItem == None:
+        return 'invalid robotname %s'%(robotname)
+
+    if thisSimulatorItem == None:
+        thisSimulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
+        if thisSimulatorItem == None:
+            return 'invalid simulatorItem'
+
+    ret = eval('thisSimulatorItem.%s()'%(call))
+
+    return '(:return %s)'%(ret)
 
 ros_service_init()
