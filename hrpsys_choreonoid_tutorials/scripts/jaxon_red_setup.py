63ef642a00fae12dd25812529451c3e12a4e7286
mttamtam <tamura@jsk.imi.i.u-tokyo.ac.jp>
2017-07-05 16:23:09 +0900

    set reset-pose angle-vector on initialize

--- hrpsys_choreonoid_tutorials/scripts/jaxon_red_setup.py
+++ hrpsys_choreonoid_tutorials/scripts/jaxon_red_setup.py
@@ -49,6 +49,9 @@ class JAXON_RED_HrpsysConfigurator(ChoreonoidHrpsysConfiguratorOrg):
         self.el_svc.setServoErrorLimit("LARM_F_JOINT1", sys.float_info.max)
         ###
         self.startAutoBalancer()
+        # Suppress limit over message and behave like real robot that always angle-vector is in seq.
+        # Latter four 0.0 are for hands.
+        self.seq_svc.setJointAngles(self.jaxonResetPose()+[0.0, 0.0, 0.0, 0.0] , 1.0)
         self.ic_svc.startImpedanceController("larm")
         self.ic_svc.startImpedanceController("rarm")
         self.startStabilizer()
