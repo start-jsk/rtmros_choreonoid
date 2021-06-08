c3eaa34a0fa3991176f2299b08bcb4b5c81aa2ed
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-06-08 18:31:09 +0900

    [hrpsys_choreonoid_tutorials/choreonoid_hrpsys_config.py] support robots which do not have abc

--- hrpsys_choreonoid_tutorials/src/hrpsys_choreonoid_tutorials/choreonoid_hrpsys_config.py
+++ hrpsys_choreonoid_tutorials/src/hrpsys_choreonoid_tutorials/choreonoid_hrpsys_config.py
@@ -28,8 +28,9 @@ class ChoreonoidHrpsysConfiguratorOrg(URATAHrpsysConfigurator):
     def setupLogger(self, maxlen=15000):
         HrpsysConfigurator.setupLogger(self, maxlen)
         self.connectLoggerPort(self.rh, 'WAIST') ##
-        self.connectLoggerPort(self.abc, 'rfsensor')
-        self.connectLoggerPort(self.abc, 'lfsensor')
+        if self.abc:
+            self.connectLoggerPort(self.abc, 'rfsensor')
+            self.connectLoggerPort(self.abc, 'lfsensor')
         for pn in filter (lambda x : re.match("Trans_", x), self.rh.ports.keys()):
             self.connectLoggerPort(self.rh, pn)
         ##self.connectLoggerPort(self.abc, 'rhsensor')
