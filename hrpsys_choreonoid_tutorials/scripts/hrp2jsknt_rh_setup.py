b8dc97b5ea19db81fb270a2a5160f381461affef
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-06-13 14:34:06 +0900

    [hrpsys_choreonoid_tutorials] add HRP2JSK*

---
+++ hrpsys_choreonoid_tutorials/scripts/hrp2jsknt_rh_setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from hrpsys_choreonoid_tutorials.hrp2_choreonoid_hrpsys_config import *
+
+if __name__ == '__main__':
+    hcf = JSKHRP2ChoreonoidHrpsysConfigurator("HRP2JSKNT")
+    if len(sys.argv) > 2 :
+        hcf.init(sys.argv[1], sys.argv[2])
+        hcf.startABSTIMP()
+    elif len(sys.argv) > 1 :
+        hcf.init(sys.argv[1])
+        hcf.startABSTIMP()
+    else :
+        hcf.init()
